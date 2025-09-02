bc6c4db7115453d3a48efb44d893db444886e89a
Gym Bigogwe <gymbigogwe@Bigogwes-iMac.local>
2025-09-02 15:03:41 +0200

    added a feauture allowing navigation to all pages through routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Dahboard } from './Components/Dahboard';
+import { Skills } from './Components/Skills';
+import { Learning } from './Components/Learning';
+import { Career } from './Components/Career';
+import NavBar from './Components/NavBar';
 function App() {
 
   return (
-    <div className='text-3xl text-blue-500'>
-      Tabiya Present
-    </div>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <main className="flex-grow p-8 bg-gray-50">
+          <Routes>
+            <Route path="/" element={<Dahboard />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/learning" element={<Learning />} />
+            <Route path="/career" element={<Career/>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
