73ff090574a72a151bee6b867e297b5498003a66
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2025-09-07 10:25:05 +0200

    resolve conflicts

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header" 
+import Footer from './Footer'
 const Layout = () => {
   return (
     <div>
       <Header />
+      <Footer />
       <Outlet />
     </div>
   )
