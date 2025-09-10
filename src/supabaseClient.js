8a1e6d20eb18ec07bce4b93e4c7403234e4c01d1
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2025-09-11 01:16:19 +0200

    feat: fetching from csv

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl = "https://wwrhzyrvnpgmnlumpygh.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3cmh6eXJ2bnBnbW5sdW1weWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MTM4MTQsImV4cCI6MjA3MjM4OTgxNH0.TROd599WsLsopG5v8R5fd8YSIJpF7yHgvPdiS1l_Xo4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
