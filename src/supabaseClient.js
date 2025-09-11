7b89484d50695f21e123bfff5ef712722fd6face
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2025-09-11 02:23:34 +0200

    fix: gitGuardians error

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wwrhzyrvnpgmnlumpygh.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3cmh6eXJ2bnBnbW5sdW1weWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MTM4MTQsImV4cCI6MjA3MjM4OTgxNH0.TROd599WsLsopG5v8R5fd8YSIJpF7yHgvPdiS1l_Xo4";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
